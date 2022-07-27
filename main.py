3d6c56bc31c58514d4dcaede560f80d6987c58bf
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2022-07-27 16:01:03 +0900

    main File

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord, os, json
+from discord.ext import commands
+
+client = commands.AutoShardedBot(command_prefix="ㄹ!")
+
+user_datadb = {}
+with open("./config/user.json", "r", encoding='UTF8') as db_json:  
+    user_datadb = json.load(db_json)
+    
+client.pool = user_datadb
+
+for ext in filter(lambda x: x.endswith('.py') and not x.startswith('_'), os.listdir('./exts')):
+    client.load_extension('exts.' + os.path.splitext(ext)[0])
+
+client.run("Token")
